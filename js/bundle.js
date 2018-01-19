b91cceeafd9a1b3de8ad97e0bb726eb96824270c
kmtownley <kathleen009@gmail.com>
2018-01-18 22:41:04 -0500

    Creates randomg blossom paths.

--- js/bundle.js
+++ js/bundle.js
@@ -67,6 +67,9 @@
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
+const Blossom = __webpack_require__(2);
+// import Blossom from "./blossom";
+
 const Game = __webpack_require__(1);
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -78,31 +81,10 @@ document.addEventListener("DOMContentLoaded", () => {
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
-  // let ctx2 = canvasEl2.getContent("2d");
-
-
-
-  // var x = canvasEl.width/2;
-  // var y = canvasEl.height-30;
-  // var dx = 2;
-  // var dy = -2;
-  // function draw() {
-  //   // drawing code
-  //   ctx.beginPath();
-  //   ctx.rect(x, y, 50, 50);
-  //   ctx.fillStyle = "#FF0000";
-  //   ctx.fill();
-  //   ctx.closePath();
-  //   x += dx;
-  //   y += dy;
-  //
-  // }
-  // setInterval(draw, 10);
-
   const game = new Game();
-  debugger
-  // game.draw(ctx);
   game.start(ctx);
+
+  window.Blossom = Blossom;
 });
 
 
@@ -112,6 +94,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const Blossom = __webpack_require__(2);
 const Background = __webpack_require__(3);
+
+
+
+
 class Game {
   constructor() {
     this.blossoms = [];
@@ -119,20 +105,35 @@ class Game {
     this.selection = null;
     this.draw = this.draw.bind(this);
     this.renderBackground = this. renderBackground.bind(this);
+    this.createBlossoms = this.createBlossoms.bind(this);
     this.xDim = 0;
     this.yDim = 0;
 
-    // for (let i = 0; i < Game.NUM_BLOSSOMS; ++i) {
-    // blossoms.push(
-    //   Blossom.randomBlossom(Game.DIM_X, Game.DIM_Y, Game.NUM_BLOSSOMS)
-    // );
-  // }
-
+    for (let i = 0; i <= Game.NUM_BLOSSOMS; ++i) {
+      this.blossoms.push(
+        this.createBlossoms()
+      );
+    }
   }
 
   start(ctx) {
-    this.draw(ctx);
     this.renderBackground(ctx);
+    this.draw(ctx);
+  }
+
+  createBlossoms() {
+    return Blossom.randomBlossom(0, Game.DIM_Y, Game.NUM_BLOSSOMS);
+  }
+
+
+  drawBlossoms(ctx) {
+    let i = 0;
+
+    return () => {
+      if (i == 40) clearInterval(this.interval);
+      this.blossoms[i].draw(ctx);
+      i += 1;
+    };
   }
 
 
@@ -140,56 +141,34 @@ class Game {
 
 
   draw(ctx) {
-  debugger
-  let dx = 0.5;
-  let dy = 0.5;
-  let  b = new Blossom(0, 0).draw(ctx, 0, 0);
-
-   ctx.drawImage(b, this.xDim, this.yDim, 125, 125);
-   const animateCallback = () => {
-
-   if (this.xDim < 500  && this.yDim < 500) {
-     this.xDim += dx;
-     this.yDim += dy;
-     this.draw(ctx);
-     } else {
-      this.xDim = 1;
-      this.yDim = 1;
-      this.draw(ctx);
-     }
-   };
-   window.requestAnimationFrame(animateCallback);
+  // let  b = new Blossom(0, 0).draw(ctx, 0, 0);
+  // new Blossom(0, 0).draw(ctx);
+    this.interval = setInterval(this.drawBlossoms(ctx).bind(this), 5000);
+  // this.blossoms.forEach(blossom => {
+  //   setTimeout(function() { blossom.draw(ctx)}, 1000);
+  //   blossom.draw(ctx);
+  // });
+   // ctx.drawImage(b, this.xDim, this.yDim, 125, 125);
+   // const animateCallback = () => {
+   //
+   // if (this.xDim < 500  && this.yDim < 300) {
+   //
+   //   this.xDim += dx;
+   //   this.yDim += dy;
+   //   this.draw(ctx);
+   //   } else {
+   //
+   //    this.xDim = 1;
+   //    this.yDim = 1;
+   //    this.draw(ctx);
+   //   }
+   // };
+   // window.requestAnimationFrame(animateCallback);
 
   }
 
   renderBackground(ctx) {
-    debugger
-
-    let bg = new Background(0, 500).scrollImage(ctx);
-    // this.img = new Image();
-    // this.img.src = './assets/images/footer_image_tower.png';
-    //
-    // this.img.onload = () => {
-    // let x = 0;
-    // let width = this.img.naturalWidth;
-    // let min = 0-width;
-    // let count = 1;
-    //
-    //   const loop = () => {
-    //     ctx.drawImage(this.img, x, 370);
-    //     ctx.drawImage(this.img, x + width, 370);
-    //     ctx.drawImage(this.img, x + width * 2, 370);
-    //     ctx.drawImage(this.img, x + width * 3, 370);
-    //     x -= count;
-    //     if (x < min) {
-    //       x = 0;
-    //     }
-    //   };
-    //   setInterval(loop, 9);
-    // };
-
-
-    // Background.scrollImage(ctx);
+    new Background(0, 500).scrollImage(ctx);
   }
 
 
@@ -197,14 +176,9 @@ class Game {
 
 }
 
-
-
-
-
-
 Game.DIM_X = 1000;
 Game.DIM_Y = 500;
-Game.NUM_BLOSSOMS = 4;
+Game.NUM_BLOSSOMS = 20;
 
 
 module.exports = Game;
@@ -228,44 +202,155 @@ DEFAULTS = {
 
 
 class Blossom {
-  constructor(ctx) {
-    debugger
-    // options.blossom = (new Image().src = "./assets/images/whole_blossom.png");
-    // options.height = DEFAULTS.HEIGHT;
-    // options.width = DEFAULTS.WIDTH;
-    // options.posx = options.posx || DEFAULTS.POSX;
-    // options.posy = options.posy || DEFAULTS.POSY;
-    // this.randomBlossom(ctx);
+  constructor(x, y) {
+    this.x = x;
+    this.y = Math.random() * 400 + 6;
+
+    // this.isOutOfBounds = this.isOutOfBounds.bind(this)
   }
-
-  // randomBlossom(maxX, maxY, numCircles) {
-  //   debugger
-  //   let blossom = new Image();
-  //   blossom.src = "./assets/images/whole_blossom.png";
-  //   return new Blossom(
-  //     blossom.onload = () => {
-  //       // ctx.fillRect(0, 0, 1000, 500);
-  //       ctx.drawImage(blossom, maxX, maxY, 125, 125);
-  //     }
-  //   );
-  //   // return blossom;
   //
-  //   };
+  // model() {
+  //   this.rot += 0.1 * this.direc;
+  //   this.x += this.xVec;
+  //   this.yVec += this.gravity;
+  //   this.y += this.yVec;
+  //   this.bounceIf();
+  // }
+  //
+  // bounceIf() {
+  //   if (this.y >= this.bottom || this.y <= this.top) {
+  //     this.y = this.bottom;
+  //     this.yVec = -1 * this.yVec;
+  //   }
+  //
+  // }
 
 
-  draw(ctx, x, y) {
+
+  draw(ctx) {
+    let increments = [1, 2];
+    let dx = 1.1;
+    let dy = 0.9;
     this.blossom = new Image();
     this.blossom.src = './assets/images/whole_blossom.png';
     this.blossom.onload = function() {
       // ctx.drawImage(blossom, 10, 10, 125, 125);
        ctx.fillRect(0, 0, 1000, 500);
-
     };
-    return this.blossom;
-  }
+    // return this.blossom;
+    ctx.drawImage(this.blossom, this.x, this.y, 125, 125 );
+    const animateCallback = () => {
+    if (this.x <= Math.random() * 320 + 3  && this.y < Math.random() * 600 + 4) {
+      this.x += dx;
+      this.y += dy;
+      this.draw(ctx);
+    } else if (this.x < 450) {
+       this.x += dx;
+       this.y -= dy;
+       this.draw(ctx);
+     } else if (this.x < 730) {
+       this.x += dx;
+       this.y += dy;
+       this.draw(ctx);
+     } else if (this.x <= 999) {
+       this.x += dx;
+       this.y -= dy;
+       this.draw(ctx);
+     } else {
+       null
+     }
+    };
+    window.requestAnimationFrame(animateCallback);
+
+   }
+
+   moveRandom(maxY) {
+     let dy = (Math.random() * 2) - 1;
+     this.y = Math.abs((this.y + (dy * 25) * 0.1) % 300);
+   }
+
+  // draw(ctx) {
+  //   debugger
+  //   let dx = 1.1;
+  //   // let dy;
+  //   if (this.dy === null) {
+  //     this.dy = (Math.random() * 4) - 1;
+  //   }
+  //
+  //   this.blossom = new Image();
+  //   this.blossom.src = './assets/images/whole_blossom.png';
+  //   this.blossom.onload = function() {
+  //     // ctx.drawImage(blossom, 10, 10, 125, 125);
+  //      ctx.fillRect(0, 0, 1000, 500);
+  //   };
+  //   // return this.blossom;
+  //   ctx.drawImage(this.blossom, this.x, this.y, 125, 125 );
+  //   const animateCallback = () => {
+  //     for (let i = 1; i < 20; i++) {
+  //       if (i === 1 && (this.y <= 150 && this.y > 0)) {
+  //         this.x += dx;
+  //         this.y += this.dy;
+  //         this.draw(ctx);
+  //       } else if ( i === 1 && (this.y >150 && this.y < 300)) {
+  //         this.x +=dx;
+  //         this.y -= this.dy;
+  //         this.draw(ctx);
+  //       } else if (this.isOutOfBounds(this.y) && this.x < 1000) {
+  //         debugger
+  //         this.reverseDy(this.dy);
+  //         this.x += dx;
+  //         this.y += this.dy;
+  //         this.draw(ctx);
+  //       } else if (this.x < 1000) {
+  //         debugger
+  //         this.x += dx;
+  //         this.y += this.dy;
+  //         this.draw(ctx);
+  //       } else {
+  //         this.x = 1;
+  //         this.y = Math.random() * 70 + 10;
+  //         this.draw(ctx);
+  //       }
+  //
+  //        }
+  //     };
+  //     window.requestAnimationFrame(animateCallback);
+  //   }
+
+
+   // reverseDy(dy) {
+   //   return (
+   //   this.dy = dy * -1
+   //   );
+   // }
+   //
+   // isOutOfBounds(y) {
+   //   if (y > 300 || y < 1 ) {
+   //     return true;
+   //   } else {
+   //     return false;
+   //   }
+   // }
+
+   // moveUp() {
+   //   if {this.starty < }
+   // }
+
 
 
 }
+
+Blossom.randomBlossom = (maxX, maxY, numBlossoms) => {
+
+  let blossom = new Image();
+  blossom.src = "./assets/images/whole_blossom.png";
+
+  return new Blossom(
+    maxX * Math.random(),
+    maxY * Math.random()
+  );
+};
+
 
 module.exports = Blossom;
 
@@ -289,7 +374,6 @@ class Background {
     let count = 1;
 
       const loop = () => {
-  
         ctx.drawImage(this.img, x, 370);
         ctx.drawImage(this.img, x + width, 370);
         ctx.drawImage(this.img, x + width * 2, 370);
@@ -302,10 +386,7 @@ class Background {
     setInterval(loop, 9);
     };
   }
-
 }
-
-
 
 module.exports = Background;
 

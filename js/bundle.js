d6b86bafbc9a8dc727f2da4abb4e5f84c9aa1fc6
kmtownley <kathleen009@gmail.com>
2018-01-19 11:57:31 -0500

    updates merge conflicts

--- js/bundle.js
+++ js/bundle.js
@@ -120,9 +120,130 @@ document.addEventListener("DOMContentLoaded", () => {
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module parse failed: Unexpected token (71:21)\nYou may need an appropriate loader to handle this file type.\n| //   }\n| \n|   renderBackground() {\n| \n|     this.interval = setInterval(this.drawBlossoms(ctx).bind(this), 3000);");
+const Blossom = __webpack_require__(2);
+const Background = __webpack_require__(3);
+
+class Game {
+  constructor() {
+    this.blossoms = [];
+    this.valid = false;
+    this.selection = null;
+    this.draw = this.draw.bind(this);
+    this.renderBackground = this. renderBackground.bind(this);
+    this.createBlossoms = this.createBlossoms.bind(this);
+    this.xDim = 0;
+    this.yDim = 0;
+    this.drawnBlossoms = [];
+
+    for (let i = 0; i <= Game.NUM_BLOSSOMS; ++i) {
+      this.blossoms.push(
+        this.createBlossoms()
+      );
+    }
+  }
+
+  beginBackground(ctx2) {
+    this.renderBackground(ctx2);
+  }
+
+  start(ctx) {
+    // this.renderBackground(ctx);
+    this.draw(ctx);
+    this.collisionHandler();
+  }
+
+  createBlossoms() {
+    return Blossom.randomBlossom(0, Game.DIM_Y, Game.NUM_BLOSSOMS);
+  }
+
+  drawBlossoms(ctx) {
+    let i = 0;
+
+    return () => {
+      if (i == 40) clearInterval(this.interval);
+      this.blossoms[i].draw(ctx);
+      debugger
+      i += 1;
+    };
+  }
+
+  draw(ctx) {
+  debugger
+  let dx = 0.5;
+  let dy = 0.5;
+  let  b = new Blossom(0, 0).draw(ctx, 0, 0);
+
+   ctx.drawImage(b, this.xDim, this.yDim, 125, 125);
+   const animateCallback = () => {
+   if (this.xDim < 500  && this.yDim < 500) {
+     this.xDim += dx;
+     this.yDim += dy;
+     this.draw(ctx);
+     } else {
+      this.xDim = 1;
+      this.yDim = 1;
+      this.draw(ctx);
+     // window.requestAnimationFrame(animateCallback);
+     }
+   };
+   window.requestAnimationFrame(animateCallback);
+  }
+
+  renderBackground() {
+
+    this.interval = setInterval(this.drawBlossoms(ctx).bind(this), 3000);
+  }
+
+
+  renderBackground(ctx2) {
+    new Background(0, 500).scrollImage(ctx2);
+  }
+
+  findVisibleBlossoms() {
+    debugger
+    this.blossoms.forEach(blossom => {
+      if (blossom.x > 0) {
+        this.visibleBlossoms.push(blossom);
+      }
+    });
+    return this.visibleBlossoms;
+  }
+
+  collisionHandler(blossom, nextBlossom) {
+    debugger
+    this.findVisibleBlossoms();
+      if (this.visibleBlossoms !== undefined) {
+      this.visibleBlossoms.forEach((blossom, idx) => {
+        let nextBlossom = this.vissibleBlossoms[idx + 1];
+        if (blossom.x > nextBlossom.x) {
+          blossom.x -= 10;
+        }
+      });
+    }
+  }
+
+
+
+//   collisionDetection() {
+//     if (rect1.x < rect2.x + rect2.width &&
+//    rect1.x + rect1.width > rect2.x &&
+//    rect1.y < rect2.y + rect2.height &&
+//    rect1.height + rect1.y > rect2.y) {
+//     // collision detected!
+// }
+//   }
+
+}
+
+Game.DIM_X = 1000;
+Game.DIM_Y = 500;
+Game.NUM_BLOSSOMS = 20;
+
+
+module.exports = Game;
+
 
 /***/ }),
 /* 2 */
@@ -332,6 +453,43 @@ Blossom.randomBlossom = (maxX, maxY, numBlossoms) => {
 
 
 module.exports = Blossom;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+class Background {
+  constructor() {
+  }
+
+  scrollImage(ctx2) {
+    this.img = new Image();
+    this.img.src = './assets/images/footer_image_tower.png';
+
+    this.img.onload = () => {
+    let x = 0;
+    let width = this.img.naturalWidth;
+    let min = 0 - width;
+    let count = 1;
+
+      const loop = () => {
+        ctx2.fillRect(0, 0, 1000, 500);
+        ctx2.drawImage(this.img, x, 370);
+        ctx2.drawImage(this.img, x + width, 370);
+        ctx2.drawImage(this.img, x + width * 2, 370);
+        ctx2.drawImage(this.img, x + width * 3, 370);
+        x -= count;
+        if (x < min) {
+          x = 0;
+        }
+      };
+    setInterval(loop, 9);
+    };
+  }
+}
+
+module.exports = Background;
 
 
 /***/ })

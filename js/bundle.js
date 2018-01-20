f356fc525da4180fa1cd34eb890c446e8476af54
kmtownley <kathleen009@gmail.com>
2018-01-20 17:08:03 -0500

    Removes comments

--- js/bundle.js
+++ js/bundle.js
@@ -68,8 +68,8 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 const Blossom = __webpack_require__(2);
-
 const Game = __webpack_require__(1);
+const Words = __webpack_require__(5);
 
 document.addEventListener("DOMContentLoaded", () => {
   let canvasEl = document.getElementById("myCanvas");
@@ -77,7 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
   canvasEl.height = 500;
   canvasLeft = canvasEl.offsetLeft;
   canvasTop = canvasEl.offsetTop;
-
 
   let ctx = canvasEl.getContext("2d");
   ctx.fillStyle = "#ffffff";
@@ -96,16 +95,6 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(x, y);
     game.receiveMouseXY(x, y);
   });
-  // canvasEl.onclick = function(e) {
-  //   // let x = e.clientX;
-  //   // let y = e.clientY;
-  //   let x = e.pageX - canvasLeft;
-  //   let y = e.pageY - canvasTop;
-  //   console.log(x, y);
-  //
-  //   game.receiveMouseXY(x, y);
-  // };
-
 
   let backgroundEl = document.getElementById("canvasBackground");
   backgroundEl.height = 100;
@@ -120,32 +109,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let ctx3 = explosionEl.getContext("2d");
 
 
-
-  // canvasEl.addEventListener('click', () => {
-  //  console.log('canvas click');
-  // });
-  // let canvasPosition = {
-  //   x: canvasEl.offset().left,
-  //   y: canvasEl.offset().top
-  // };
-  //
-  // canvasEl.on('click', function(e) {
-  //
-  //   // use pageX and pageY to get the mouse position
-  //   // relative to the browser window
-  //
-  //   var mouse = {
-  //       x: e.pageX - canvasPosition.x,
-  //       y: e.pageY - canvasPosition.y
-  //   };
-  // });
-
   const game = new Game(ctx, ctx2, ctx3, ctxWords);
 
   game.beginBackground(ctx2);
   game.start(ctx, ctx3);
 
-  window.Blossom = Blossom;
+  // window.Blossom = Blossom;
 
 });
 
@@ -282,10 +251,9 @@ module.exports = Game;
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
-BLOSSOM = new Image();
-BLOSSOM.src = "./assets/images/whole_blossom.png";
+// BLOSSOM = new Image();
+// BLOSSOM.src = "./assets/images/whole_blossom.png";
 const Word = __webpack_require__(5);
-
 const Explosion = __webpack_require__(4);
 
 
@@ -302,29 +270,7 @@ class Blossom {
     this.blossomExploded = false;
     this.renderBlossom = this.renderBlossom.bind(this);
     this.decimalValue = (new Word()).decimalValue;
-    // this.decimalValue =
-
-    // this.isOutOfBounds = this.isOutOfBounds.bind(this)
   }
-  //
-  // model() {
-  //   this.rot += 0.1 * this.direc;
-  //   this.x += this.xVec;
-  //   this.yVec += this.gravity;
-  //   this.y += this.yVec;
-  //   this.bounceIf();
-  // }
-  //
-  // bounceIf() {
-  //   if (this.y >= this.bottom || this.y <= this.top) {
-  //     this.y = this.bottom;
-  //     this.yVec = -1 * this.yVec;
-  //   }
-  //
-  // }
-
-
-
 
   draw(ctx) {
     let increments = [1, 2];
@@ -333,8 +279,6 @@ class Blossom {
     this.blossom = new Image();
     this.blossom.src = './assets/images/whole_blossom.png';
     this.blossom.onload = function() {
-      // ctx.drawImage(blossom, 10, 10, 125, 125);
-       // ctx.fillRect(0, 0, 1000, 500);
     };
     if (this.blossomExlpoded === true) {
 
@@ -344,19 +288,8 @@ class Blossom {
           ctx.putImageData(this.blossom, this.x, this.y);
       }
 
-    // ctx.onclick = () => {
-    //
-    //   // let mouseX = e.clientX;
-    //   // let mouseY = e.clientY;
-    //   console.log("yay");
-    // };
-    // return this.blossom;
     ctx.drawImage(this.blossom, this.x, this.y, 125, 125 );
-    // ctx.font = '40pt Calibri';
-    // ctx.fillText(this.decimalValue, 18, 18);
-
     ctx.font="24px Varela Round";
-    // ctx.fillStyle
     ctx.fillText(this.decimalValue, this.x + 37, this.y + 73);
 
 
@@ -365,15 +298,12 @@ class Blossom {
         if (this.x <= Math.random() * 320 + 3  && this.y < Math.random() * 600 + 4) {
           this.x += dx;
           this.y += dy;
-
           this.renderBlossom(ctx);
         } else if (this.x < 400) {
-
            this.x += dx;
            this.y -= dy;
            this.renderBlossom(ctx);
          } else if (this.x < 730) {
-
            this.x += dx;
            this.y += dy;
            this.renderBlossom(ctx);
@@ -387,7 +317,6 @@ class Blossom {
        }
     };
     window.requestAnimationFrame(animateCallback);
-
    }
 
    renderBlossom() {
@@ -413,7 +342,6 @@ class Blossom {
    // }
 
    collisionDetected(blossom, nextBlossom) {
-
      this.findVisibleBlossoms();
        if (this.visibleBlossoms !== undefined) {
        this.visibleBlossoms.forEach((blossom, idx) => {
@@ -421,16 +349,8 @@ class Blossom {
          if (blossom.x > nextBlossom.x) {
            return true;
          }
-
        });
      }
-
-     // return !(
-     // (this.x + this.height) < otherBlossom.y ||
-     //    (this.y > (otherBlossom.y + otherBlossom.height)) ||
-     //   ((this.x + this.width) < otherBlossom.x) ||
-     //   (this.x > (otherBlosom.x + otherBlossom.width))
-     // );
    }
 
    moveRandom(maxY) {
@@ -438,116 +358,23 @@ class Blossom {
      this.y = Math.abs((this.y + (dy * 25) * 0.1) % 300);
    }
 
-   messageUser() {
-
-     console.log("You clicked me");
-
-   }
-
    explodeBlossom() {
-
      let explosion = new Explosion(this.x, this.y, this.ctx3);
      explosion.drawSprite();
-
-     // let currentFrame = 0;  // the current frame to draw
-     // let counter = 0;
-     // let exlpodeImage = new Image();
-     // let framesPerRow;
-     // this.explodeImage.onload = () => {
-     //   framesPerRow = Math.floor(explodeImage.width / 128);
-     // };
-     //
-     // explodImage.src = "../assets/images/explosion_sprite.png";
    }
 
-   // updateSprite() {
-   //   if (counter === (frameSpeed - 1))
-   //    currentFrame = (currentFrame + 1) % 1020;
-   //
-   //  // update the counter
-   //  counter = (counter + 1) % frameSpeed;
-   //  }// keep track of frame rate
-   // }
-  // draw(ctx) {
-  //
-  //   let dx = 1.1;
-  //   // let dy;
-  //   if (this.dy === null) {
-  //     this.dy = (Math.random() * 4) - 1;
-  //   }
-  //
-  //   this.blossom = new Image();
-  //   this.blossom.src = './assets/images/whole_blossom.png';
-  //   this.blossom.onload = function() {
-  //     // ctx.drawImage(blossom, 10, 10, 125, 125);
-  //      ctx.fillRect(0, 0, 1000, 500);
-  //   };
-  //   // return this.blossom;
-  //   ctx.drawImage(this.blossom, this.x, this.y, 125, 125 );
-  //   const animateCallback = () => {
-  //     for (let i = 1; i < 20; i++) {
-  //       if (i === 1 && (this.y <= 150 && this.y > 0)) {
-  //         this.x += dx;
-  //         this.y += this.dy;
-  //         this.draw(ctx);
-  //       } else if ( i === 1 && (this.y >150 && this.y < 300)) {
-  //         this.x +=dx;
-  //         this.y -= this.dy;
-  //         this.draw(ctx);
-  //       } else if (this.isOutOfBounds(this.y) && this.x < 1000) {
-  //
-  //         this.reverseDy(this.dy);
-  //         this.x += dx;
-  //         this.y += this.dy;
-  //         this.draw(ctx);
-  //       } else if (this.x < 1000) {
-  //
-  //         this.x += dx;
-  //         this.y += this.dy;
-  //         this.draw(ctx);
-  //       } else {
-  //         this.x = 1;
-  //         this.y = Math.random() * 70 + 10;
-  //         this.draw(ctx);
-  //       }
-  //
-  //        }
-  //     };
-  //     window.requestAnimationFrame(animateCallback);
-  //   }
 
 
-   // reverseDy(dy) {
-   //   return (
-   //   this.dy = dy * -1
-   //   );
-   // }
-   //
-   // isOutOfBounds(y) {
-   //   if (y > 300 || y < 1 ) {
-   //     return true;
-   //   } else {
-   //     return false;
-   //   }
-   // }
-
-   // moveUp() {
-   //   if {this.starty < }
-   // }
    removeBlossom() {
 
      delete this;
    }
 
-
-
 }
 
 Blossom.randomBlossom = (maxX, maxY, numBlossoms, ctx3, ctx) => {
-
   let blossom = new Image();
   blossom.src = "./assets/images/whole_blossom.png";
-
   return new Blossom(
     maxX * Math.random(),
     maxY * Math.random(),
@@ -664,7 +491,7 @@ class Explosion {
       //
       // this.explodeImage.startPos += 128;
       // window.requestAnimationFrame(this.animateCallback(ctx3).bind(this));
-      this.ctx3.drawImage(this.explodeImage, this.explodeImage.startPosX, this.explodeImage.startPosY, 128, 128, this.blossomX / 8.3, this.blossomY / 5.9, 30, 70);
+      this.ctx3.drawImage(this.explodeImage, this.explodeImage.startPosX, this.explodeImage.startPosY, 128, 128, this.blossomX / 9.2, this.blossomY / 5.9, 30, 70);
       this.explodeImage.startPosX += 128;
 
       if (this.explodeImage.startPosX >= 1024 & this.explodeImage.count <= 4) {

4744d2e6c6394c96cdc13ccdcd0a4c4d7531e4e1
kmtownley <kathleen009@gmail.com>
2018-01-21 12:30:33 -0500

    Creates navbar functionality and modal

--- js/bundle.js
+++ js/bundle.js
@@ -82,7 +82,6 @@ document.addEventListener("DOMContentLoaded", () => {
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
-
   let canvasWords = document.getElementById("canvasWords");
   canvasWords.width = 80;
   canvasWords.height = 80;
@@ -94,6 +93,27 @@ document.addEventListener("DOMContentLoaded", () => {
     let y = e.pageY - canvasTop;
     game.receiveMouseXY(x, y);
   });
+
+  let modal = document.getElementById('myModal');
+  // Get the button that opens the modal
+  let btn = document.getElementById("myBtn");
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "block";
+  };
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  };
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  };
 
   let backgroundEl = document.getElementById("canvasBackground");
   backgroundEl.height = 100;
@@ -136,7 +156,7 @@ class Game {
     this.draw = this.draw.bind(this);
     // this.renderBackground = this. renderBackground.bind(this);
     this.createBlossoms = this.createBlossoms.bind(this);
-    // this.findVisibleBlossoms = this.findVisibleBlossoms.bind(this);
+    this.findVisibleWords = this.findVisibleWords.bind(this);
     this.xDim = 0;
     this.yDim = 0;
     this.ctx3 = ctx3;
@@ -161,8 +181,27 @@ class Game {
     // let word = new Word();
     // word.findWord();
     // let player = new Player();
+    // this.findVisibleWords();
     Word.prototype.renderWordChoice(this.visibleWords);
   }
+
+//   var startTime, endTime;
+//
+//   start() {
+//   let startTime = new Date();
+//   };
+//
+//   end() {
+//   endTime = new Date();
+//   let timeDiff = endTime - startTime; //in ms
+//   // strip the ms
+//   timeDiff /= 1000;
+//
+//   // get seconds
+//   var seconds = Math.round(timeDiff);
+//   console.log(seconds + " seconds");
+// }
+
 
   createBlossoms() {
     return Blossom.randomBlossom(0, Game.DIM_Y, Game.NUM_BLOSSOMS, this.ctx3, this.ctx);
@@ -179,7 +218,7 @@ class Game {
   }
 
   draw() {
-    this.interval = setInterval(this.drawBlossoms(this.ctx).bind(this), 2500);
+    this.interval = setInterval(this.drawBlossoms(this.ctx).bind(this), 3000);
   }
 
   renderBackground(ctx2) {
@@ -201,6 +240,7 @@ class Game {
         Word.prototype.renderWordChoice(this.visibleWords);
       }
     });
+    // setInterval(this.findVisibleWords, 3000);
 
   }
   //
@@ -270,7 +310,7 @@ class Game {
 
 Game.DIM_X = 1000;
 Game.DIM_Y = 500;
-Game.NUM_BLOSSOMS = 30;
+Game.NUM_BLOSSOMS = 50;
 
 module.exports = Game;
 
